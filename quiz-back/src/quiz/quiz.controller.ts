0fb7bc220520d3349b2ced628d40e502c4687807
jbarus <57799873+jbarus@users.noreply.github.com>
2024-01-08 09:22:50 +0100

    Secured needed routes and added more quiz functionalities

--- quiz-back/src/quiz/quiz.controller.ts
+++ quiz-back/src/quiz/quiz.controller.ts
@@ -1,21 +1,45 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { Quiz } from '@prisma/client';
+import { BasicGuard } from '../auth/basic.guard';
+import { AuthService } from '../auth/auth.service';
+import { TokenGuard } from '../auth/token.guard';
+import { UserID } from '../auth/user.decorator';
 
 @Controller('quiz')
 export class QuizController {
-  constructor(private quizService: QuizService) {}
-  @Get(':id')
+  constructor(
+    private quizService: QuizService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('random')
+  getRandomQuiz(@Query('amount') amount: string) {
+    return this.quizService.getRandomQuiz(parseInt(amount));
+  }
+  @Get('/:id')
+  @UseGuards(TokenGuard)
   hello(@Param('id') id: string) {
     return this.quizService.getQuizById(id);
   }
 
   @Post()
-  saveQuiz(@Body() quiz: Quiz) {
-    return this.quizService.saveQuiz(quiz);
+  @UseGuards(TokenGuard)
+  saveQuiz(@UserID() userId: string, @Body() quiz: Quiz) {
+    return this.quizService.saveQuiz(userId, quiz);
   }
-  @Get('random')
-  getRandomQuiz() {
-    return this.quizService.getRandomQuiz();
+
+  @Get()
+  @UseGuards(TokenGuard)
+  getUserQuiz(@UserID() userId: string) {
+    return this.quizService.getUserQuiz(userId);
   }
 }
--- quiz-back/src/quiz/quiz.service.ts
+++ quiz-back/src/quiz/quiz.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Quiz } from '@prisma/client';
+import { Quiz, User } from '@prisma/client';
 
 @Injectable({})
 export class QuizService {
   constructor(private prisma: PrismaService) {}
 
-  saveQuiz(quiz: Quiz) {
-    return this.prisma.quiz.create({ data: quiz });
+  async saveQuiz(userId: string, quiz: Quiz) {
+    const savedQuiz = await this.prisma.quiz.create({ data: quiz });
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { quizId: { push: savedQuiz.id } },
+    });
+    return savedQuiz;
   }
 
   getQuizById(id: string) {
@@ -18,7 +23,26 @@ export class QuizService {
     });
   }
 
-  getRandomQuiz() {
-    return this.prisma.quiz.findMany({ take: 1 });
+  getRandomQuiz(amount: number) {
+    return this.prisma.quiz.findMany({ take: amount });
+  }
+
+  async getUserQuiz(userId: string) {
+    const quizzes = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: { quizId: true },
+    });
+    const quizIds = quizzes?.quizId || [];
+
+    const quizTable: {
+      id: string;
+      title: string;
+      description: string;
+      category: string;
+    }[] = [];
+    return this.prisma.quiz.findMany({
+      where: { id: { in: quizIds } },
+      select: { id: true, title: true, description: true, category: true },
+    });
   }
 }
